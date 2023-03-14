9e76cf7323db3d27e20dec9c31f8a25032cb3491
Jondasneves <jonasjose1479@gmail.com>
2023-03-14 10:54:30 -0300

    fala

--- nova.py
+++
@@ -1,8 +0,0 @@
-import speech_recognition as sr
-rec = sr.Recognizer()
-with sr.Microphone() as mic:
-    rec.adjust_for_ambient_noise(mic)
-    print ("fala ai")
-    audio = rec.listen(mic)
-    texto = rec.recognize_amazon(audio, language= "pt-BR")
-    print(texto)
